632c2c572f43343c58e98bb0fdb24bb39d850ada
Boudewijn van Groos <boudewijn@vangroos.nl>
2016-10-06 21:20:40 +0200

    Expand pizzas model to include order

--- frontend/app/routes/pizzas.js
+++ frontend/app/routes/pizzas.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
   model() {
-    return this.get('store').findAll('pizza');
+    return RSVP.hash({
+      pizzas: this.get('store').findAll('pizza'),
+      order: this.get('store').createRecord('order')
+    });
   }
 });
