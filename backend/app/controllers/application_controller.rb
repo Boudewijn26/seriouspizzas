97545d366d7eb35a50e726996349130007413da8
Boudewijn van Groos <boudewijn@vangroos.nl>
2016-10-07 12:56:32 +0200

    Config rails to use rack-cors

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,11 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :cors_headers
-
-  def cors_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'PATCH, POST, PUT, DELETE, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  end
-
 end
