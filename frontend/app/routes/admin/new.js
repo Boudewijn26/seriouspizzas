fc178f98c04ce70cf1fea7ece26fd2e7cd674d79
Boudewijn van Groos <boudewijn@vangroos.nl>
2016-10-08 21:21:52 +0200

    Use ember data route to clean up unsaved records

--- frontend/app/routes/admin/new.js
+++ frontend/app/routes/admin/new.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import DataRoute from 'ember-data-route';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(DataRoute, {
   model() {
     return this.get('store').createRecord('pizza', {});
   }
