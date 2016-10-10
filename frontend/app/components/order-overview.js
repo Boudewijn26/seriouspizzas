8bf2f6e00d6e0ed806127b1d2879c17ce465befa
Boudewijn van Groos <boudewijn@vangroos.nl>
2016-10-10 11:53:39 +0200

    Allow orders to be removed

--- frontend/app/components/order-overview.js
+++ frontend/app/components/order-overview.js
@@ -4,6 +4,9 @@ export default Ember.Component.extend({
   actions: {
     placeOrder() {
       this.get('place')(this.get('order'));
+    },
+    clearPizzaOrder(pizzaOrder) {
+      this.get('order.pizzaOrders').removeObject(pizzaOrder);
     }
   }
 });
