cea7b42d4f5226b3e1284b61e1a3548ade655d4d
Boudewijn van Groos <boudewijn@vangroos.nl>
2016-10-10 10:31:34 +0200

    Dropping users

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161010080720) do
+ActiveRecord::Schema.define(version: 20161010082825) do
 
   create_table "orders", force: :cascade do |t|
     t.integer  "user_id"
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "created_at"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
@@ -35,12 +35,6 @@ ActiveRecord::Schema.define(version: 20161010080720) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
